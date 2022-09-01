fd93caaaec8f14c4372f01083ed8f0a097dc1249
Felix Angelico Sakuayela <f.sakuayela@gmail.com>
2022-09-01 04:00:24 -0300

    Custome Send Password Reset Email

--- angel_ecommerce/account/urls.py
+++ angel_ecommerce/account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from account.views import UserRegistrationView, UserLoginView, UserProfileView, UserChangePasswordView
+from account.views import UserRegistrationView, UserLoginView, UserProfileView, UserChangePasswordView, SendPasswordResetEmailView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
-    path('changepassword/', UserChangePasswordView.as_view(), name='changepassword')
+    path('changepassword/', UserChangePasswordView.as_view(), name='changepassword'),
+    path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name='send-reset-password-email'),
 ]
--- angel_ecommerce/account/views.py
+++ angel_ecommerce/account/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from account.serializers import UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer, UserChangePasswordSerializer
+from account.serializers import UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer, UserChangePasswordSerializer, SendPasswordResetEmailSerializer
 from django.contrib.auth import authenticate
 from account.renderers import UserRenderer
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -55,3 +55,10 @@ class UserChangePasswordView(APIView):
         serializer = UserChangePasswordSerializer(data=request.data, context={'user':request.user})
         serializer.is_valid(raise_exception=True)
         return Response({'msg':'Password Changed Successfully'}, status=status.HTTP_200_OK)
+
+class SendPasswordResetEmailView(APIView):
+    renderer_classes = [UserRenderer]
+    def post(self, request, format=None):
+        serializer = SendPasswordResetEmailSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response({'msg':'Password Reset link send. Please check your Email'}, status=status.HTTP_200_OK)
