f215441e3cabb81416452b59f7606c537acb3c9e
Felix Angelico Sakuayela <f.sakuayela@gmail.com>
2022-09-07 16:55:10 -0300

    Fix Code - User Registration View

--- angel_ecommerce/account/views.py
+++ angel_ecommerce/account/views.py
@@ -17,14 +17,13 @@ def get_tokens_for_user(user):
 
 
 class UserRegistrationView(APIView):
-    renderer_class = [UserRenderer]
+    renderer_classes = [UserRenderer]
     def post(self, request, format=None):
         serializer = UserRegistrationSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            user = serializer.save()
-            token = get_tokens_for_user(user)
-            return Response({'token':token,'msg': 'Registration Successful'}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = get_tokens_for_user(user)
+        return Response({'token':token, 'msg':'Registration Successful'}, status=status.HTTP_201_CREATED)
 
 class UserLoginView(APIView):
     renderer_classes = [UserRenderer]
